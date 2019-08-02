d0d806b3e72c6b6469b96d71d88a0a3f1b33f9a3
*VogtLe 威廉* <48601875+VogtLeJato@users.noreply.github.com>
2019-08-02 10:14:23 -0300

    Create exercicio1php.php

---
+++ exercicio1php.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Resposta</title>
+
+    </head>
+
+    <body>
+
+            <h1>Sucesso!</h1>
+            <?php
+    $carro= $_POST["modelo"];
+    $cor = $_POST["cor"];
+    print "Seu $carro de cor $cor está pronto. Dirija com cuidado!"; ?>
+            
+
+    </body>
+</html>
